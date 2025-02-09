fbde3251099433311e8378bb1ec21e6aeaff4cef
rishabh6900 <prajapatirishabh071@gmail.com>
2025-02-09 14:15:51 +0530

    util

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.cnnClassifier import logger
+# from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+
+logger.info("welcome to our custom log")
